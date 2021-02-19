e2a0a2f2c6acc548d8ac847d5fb48c58d245d725
Julien Aubert <julien.aubert.rbs@gmail.com>
2021-02-19 12:08:29 +0100

    Login route creation

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,40 @@
-const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
+// Firebase
+const functions = require('firebase-functions');
+const { admin, db } = require('./admin');
+const config = require('./config');
+const firebase = require('firebase/app');
+firebase.initializeApp(config);
+
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
-
-// The Firebase Admin SDK to access Firestore.
-const admin = require('firebase-admin');
-admin.initializeApp();
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info('Hello logs!', { structuredData: true });
-//   response.send('Hello World!');
-// });
 
 app.get('/helloworld', (request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true });
   response.send('Hello World!');
+});
+
+app.post('/login', (req, res) => {
+  const userData = {
+    email: req.body.email,
+    password: req.body.email,
+  };
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(userData.email, userData.password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      return res.status(200).json('Login succesful');
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      return res.status(404).json(errorCode, errorMessage);
+    });
 });
 
 // Take the text parameter passed to this HTTP endpoint and insert it into
@@ -29,32 +43,11 @@ app.get('/addMessage', async (req, res) => {
   // Grab the text parameter.
   const original = req.query.text;
   // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin
-    .firestore()
+  const writeResult = await db
     .collection('messages')
     .add({ original: original });
   // Send back a message that we've successfully written the message
   res.json({ result: `Message with ID: ${writeResult.id} added.` });
 });
 
-// Listens for new messages added to /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-exports.makeUppercase = functions.firestore
-  .document('/messages/{documentId}')
-  .onCreate((snap, context) => {
-    // Grab the current value of what was written to Firestore.
-    const original = snap.data().original;
-
-    // Access the parameter `{documentId}` with `context.params`
-    functions.logger.log('Uppercasing', context.params.documentId, original);
-
-    const uppercase = original.toUpperCase();
-
-    // You must return a Promise when performing asynchronous tasks inside a Functions such as
-    // writing to Firestore.
-    // Setting an 'uppercase' field in Firestore document returns a Promise.
-    return snap.ref.set({ uppercase }, { merge: true });
-  });
-
-// Expose Express API as a single Cloud Function:
-exports.api = functions.https.onRequest(app);
+exports.api = functions.region('europe-west1').https.onRequest(app);
