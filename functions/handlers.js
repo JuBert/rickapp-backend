7dbaf232209be35de8c2ae08832e5bf556fa0297
Julien Aubert <julien.aubert.rbs@gmail.com>
2021-02-22 18:28:18 +0100

    GetBeer route, refactored routes

---
+++ functions/handlers.js
@@ -0,0 +1,58 @@
+// Firebase
+const { admin, db } = require('./util/admin');
+const config = require('./util/config');
+require('firebase/auth');
+const functions = require('firebase-functions');
+const firebase = require('firebase/app');
+firebase.initializeApp(config);
+// Axios
+const axios = require('axios');
+
+exports.helloworld = (req, res) => {
+  functions.logger.info('Hello logs!', { structuredData: true });
+  res.send(firebase.apps.map((e) => e.name));
+};
+
+exports.login = (req, res) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(req.body.email, req.body.password)
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      return res.json({ token });
+    })
+    .catch((error) => {
+      console.log(error);
+      return res
+        .status(403)
+        .json((general = 'Wrong credentials, please try again'));
+    });
+};
+
+// Take the text parameter passed to this HTTP endpoint and insert it into
+// Firestore under the path /messages/:documentId/original
+exports.addMessage = async (req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await db
+    .collection('messages')
+    .add({ original: original });
+  // Send back a message that we've successfully written the message
+  res.json({ result: `Message with ID: ${writeResult.id} added.` });
+};
+
+exports.getBeers = (req, res) => {
+  axios
+    .get('https://api.punkapi.com/v2/beers?per_page=3')
+    .then((response) => {
+      let beers = [];
+      response.data.map((beer) => {
+        beers.push(beer);
+      });
+      res.status(200).json(beers);
+    })
+    .catch((err) => res.status(500).json({ error: err.code }));
+};
--- functions/index.js
+++ functions/index.js
@@ -3,47 +3,14 @@ const cors = require('cors');
 const app = express();
 app.use(cors({ origin: true }));
 // Firebase
-const firebase = require('firebase/app');
-require('firebase/auth');
 const functions = require('firebase-functions');
-const { admin, db } = require('./util/admin');
-const config = require('./util/config');
-firebase.initializeApp(config);
+// Handlers
+const { helloworld, login, addMessage, getBeers } = require('./handlers');
 
-app.get('/helloworld', (req, res) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  res.send(firebase.apps.map((e) => e.name));
-});
-
-app.post('/login', (req, res) => {
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(req.body.email, req.body.password)
-    .then((data) => {
-      return data.user.getIdToken();
-    })
-    .then((token) => {
-      return res.json({ token });
-    })
-    .catch((error) => {
-      console.log(error);
-      return res
-        .status(403)
-        .json((general = 'Wrong credentials, please try again'));
-    });
-});
-
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-app.get('/addMessage', async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await db
-    .collection('messages')
-    .add({ original: original });
-  // Send back a message that we've successfully written the message
-  res.json({ result: `Message with ID: ${writeResult.id} added.` });
-});
+// Routes
+app.get('/helloworld', helloworld);
+app.get('/addMessage', addMessage);
+app.post('/login', login);
+app.get('/beers', getBeers);
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
